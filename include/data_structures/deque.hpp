b268ff99ae80a35e3dfd9cb3b6345fdb32e4b48a
Alexander Engström <aleen361@student.liu.se>
2023-10-26 10:59:23 +0200

    worked on queue

---
+++ include/data_structures/deque.hpp
@@ -0,0 +1,55 @@
+#ifndef NOT_STD_DEQUE_HPP
+#define NOT_STD_DEQUE_HPP
+
+#include "../common/common_defs.hpp"
+
+namespace not_std
+{
+
+    template <typename T>
+    class deque
+    {
+    public:
+        deque();
+        ~deque();
+
+        int check_integrity() const;
+
+        T &front() const;
+        T &back() const;
+
+        void push_front(const T &value);
+        void push_back(const T &value);
+
+        void pop_front();
+        void pop_back();
+
+        u_int size() const;
+        u_int empty() const;
+
+    private:
+        static const u_int BLOCK_SIZE = 16;
+
+        T **data;
+        u_int block_count;
+        u_int max_blocks;
+
+        T *front_block;
+        T *back_block;
+
+        u_int front_index;
+        u_int back_index;
+
+        u_int current_size;
+
+        void allocate_front();
+        void allocate_back();
+        void deallocate_front();
+        void deallocate_back();
+    };
+
+#include "impl/deque_impl.hpp"
+
+}
+
+#endif
---
+++ include/data_structures/impl/deque_impl.hpp
@@ -0,0 +1,235 @@
+template <typename T>
+not_std::deque<T>::deque() : data{nullptr},
+                             block_count{0},
+                             max_blocks{BLOCK_SIZE},
+                             front_block{nullptr},
+                             back_block{nullptr},
+                             front_index{0},
+                             back_index{0},
+                             current_size{0}
+
+{
+    data = new T *[BLOCK_SIZE];
+    for (u_int i{0}; i < BLOCK_SIZE; ++i)
+    {
+        data[i] = nullptr;
+    }
+
+    allocate_back();
+    front_block = back_block;
+}
+
+template <typename T>
+not_std::deque<T>::~deque()
+{
+    for (u_int i{0}; i < block_count; ++i)
+    {
+        delete[] data[i];
+    }
+    delete[] data;
+}
+
+template <typename T>
+T &not_std::deque<T>::front() const
+{
+    return front_block[front_index];
+}
+
+template <typename T>
+T &not_std::deque<T>::back() const
+{
+    if (back_index == 0)
+    {
+        return back_block[BLOCK_SIZE - 1];
+    }
+    else
+    {
+        return back_block[back_index - 1];
+    }
+}
+
+template <typename T>
+void not_std::deque<T>::push_front(const T &value)
+{
+    if (front_index == 0)
+    {
+        allocate_front();
+        front_index = BLOCK_SIZE - 1;
+    }
+    else
+    {
+        --front_index;
+    }
+    front_block[front_index] = value;
+    ++current_size;
+}
+
+template <typename T>
+void not_std::deque<T>::push_back(const T &value)
+{
+    back_block[back_index] = value;
+    ++current_size;
+
+    if (back_index == BLOCK_SIZE - 1)
+    {
+        allocate_back();
+        back_index = 0;
+    }
+    else
+    {
+        ++back_index;
+    }
+}
+
+template <typename T>
+void not_std::deque<T>::pop_front()
+{
+    ++front_index;
+    if (front_index == BLOCK_SIZE)
+    {
+        deallocate_front();
+        front_index = 0;
+    }
+    --current_size;
+}
+
+template <typename T>
+void not_std::deque<T>::pop_back()
+{
+    if (back_index == 0)
+    {
+        deallocate_back();
+        back_index = BLOCK_SIZE - 1;
+    }
+    else
+    {
+        --back_index;
+    }
+    --current_size;
+}
+
+template <typename T>
+u_int not_std::deque<T>::size() const
+{
+    return current_size;
+}
+
+template <typename T>
+u_int not_std::deque<T>::empty() const
+{
+    return current_size == 0;
+}
+
+template <typename T>
+void not_std::deque<T>::allocate_front()
+{
+    if (block_count == max_blocks)
+    {
+        T **new_data = new T *[max_blocks + BLOCK_SIZE];
+        for (u_int i{0}; i < block_count; ++i)
+        {
+            new_data[i + 1] = data[i];
+        }
+        delete[] data;
+        data = new_data;
+
+        max_blocks += BLOCK_SIZE;
+    }
+
+    data[0] = new T[BLOCK_SIZE];
+    ++block_count;
+    front_block = data[0];
+}
+
+template <typename T>
+void not_std::deque<T>::allocate_back()
+{
+    if (block_count == max_blocks)
+    {
+        T **new_data = new T *[max_blocks + BLOCK_SIZE];
+        for (u_int i{0}; i < block_count; ++i)
+        {
+            new_data[i] = data[i];
+        }
+        new_data[block_count - 1] = data[block_count - 1];
+
+        delete[] data;
+        data = new_data;
+
+        max_blocks += BLOCK_SIZE;
+    }
+
+    data[block_count] = new T[BLOCK_SIZE];
+    back_block = data[block_count];
+    ++block_count;
+}
+
+template <typename T>
+void not_std::deque<T>::deallocate_front()
+{
+    delete[] data[0];
+
+    for (u_int i{0}; i < block_count - 1; ++i)
+    {
+        data[i] = data[i + 1];
+    }
+
+    data[block_count - 1] = nullptr;
+    --block_count;
+    if (block_count == 0)
+    {
+        front_block = nullptr;
+        back_block = nullptr;
+    }
+    else
+    {
+        front_block = data[0];
+    }
+}
+
+template <typename T>
+void not_std::deque<T>::deallocate_back()
+{
+    delete[] data[block_count - 1];
+    data[block_count - 1] = nullptr;
+    --block_count;
+
+    if (block_count == 0)
+    {
+        front_block = nullptr;
+        back_block = nullptr;
+    }
+    else
+    {
+        back_block = data[block_count - 1];
+    }
+}
+
+template <typename T>
+int not_std::deque<T>::check_integrity() const
+{
+    if (block_count > max_blocks)
+    {
+        return 1;
+    }
+
+    if (data == nullptr && (block_count != 0 || max_blocks != BLOCK_SIZE))
+    {
+        return 2;
+    }
+
+    for (u_int i = block_count; i < max_blocks; ++i)
+    {
+        if (data[i] != nullptr)
+        {
+            return 3;
+        }
+    }
+
+    if (current_size == 0 && (front_block != back_block || front_index != back_index))
+    {
+        return 4;
+    }
+
+    return 0;
+}
